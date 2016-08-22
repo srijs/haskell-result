145084516883bc85163e66bcb1b2fc45dd07cdd8
Sam Rijs <srijs@airpost.net>
2016-08-22 18:10:11 +0800

    add a few instances

--- src/Data/Result.hs
+++ src/Data/Result.hs
@@ -7,14 +7,26 @@ module Data.Result
   ) where
 
 
-import Data.Monoid
+import Data.Bifunctor
+import Data.Bifoldable
+import Data.Bitraversable
 import Data.Key
 import qualified Data.List.NonEmpty as NonEmpty
+import Data.Monoid
 
 
 newtype Result e a =
     Result (Either (NonEmpty.NonEmpty e) a)
-  deriving (Eq, Ord, Show, Functor, Applicative, Monad)
+  deriving (Eq, Ord, Show, Functor, Applicative, Monad, Foldable)
+
+
+instance Traversable (Result e) where
+  traverse f (Result e) =
+    case e of
+      Left es ->
+        pure (Result (Left es))
+      Right a ->
+        Result . Right <$> f a
 
 
 instance Zip (Result e) where
@@ -24,6 +36,33 @@ instance Zip (Result e) where
         Result (Right (f eaa eba))
       _ ->
         Result (Left (NonEmpty.fromList (errors mea <> errors meb)))
+
+
+instance Bifunctor Result where
+  bimap f g (Result e) =
+    case e of
+      Left es ->
+        Result (Left (fmap f es))
+      Right a ->
+        Result (Right (g a))
+
+
+instance Bifoldable Result where
+  bifoldMap f g (Result e) =
+    case e of
+      Left es ->
+        foldMap f es
+      Right a ->
+        g a
+
+
+instance Bitraversable Result where
+  bitraverse f g (Result e) =
+    case e of
+      Left es ->
+        Result . Left <$> traverse f es
+      Right a ->
+        Result . Right <$> g a
 
 
 raise :: e -> Result e a
